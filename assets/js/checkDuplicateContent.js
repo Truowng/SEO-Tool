eb06a376dcae0df89fd025689007ab398ed084d1
DESKTOP-MKH8SQO\quock <quockhanhtruong166@gmail.com>
2022-10-21 15:53:30 +0700

    add case check keyword

--- assets/js/checkDuplicateContent.js
+++ assets/js/checkDuplicateContent.js
@@ -1,9 +1,18 @@
 function checkDuplicateContentFn(value, valueCheck) {
   const duplicateContent = document.querySelector(".duplicate-content");
-  let duplicateContentTimes =
-    value.toLowerCase().split(valueCheck.toLowerCase()).length - 1;
-  duplicateContent.innerHTML = `<span>Số lần lặp từ là:</span> ${duplicateContentTimes}`;
-  console.log(duplicateContentTimes);
+  if (value.length !== 0 && valueCheck.length !== 0) {
+    console.log("hello");
+    let duplicateContentTimes =
+      value.toLowerCase().split(valueCheck.toLowerCase()).length - 1;
+    duplicateContent.innerHTML = `<span>Số lần lặp từ là:</span> ${duplicateContentTimes}`;
+    console.log(duplicateContentTimes);
+  } else if (value.length === 0 && valueCheck.length !== 0) {
+    duplicateContent.innerHTML = `<span>Số lần lặp từ là:</span> Vui lòng nhập đoạn văn.`;
+  } else if (value.length !== 0 && valueCheck.length === 0) {
+    duplicateContent.innerHTML = `<span>Số lần lặp từ là:</span> Vui lòng nhập từ khóa.`;
+  } else {
+    duplicateContent.innerHTML = `<span>Số lần lặp từ là:</span> Vui lòng nhập đoạn văn và từ khóa.`;
+  }
 }
 
 export { checkDuplicateContentFn };
--- assets/js/checkTitleLength.js
+++ assets/js/checkTitleLength.js
@@ -5,7 +5,7 @@ function checkTitleLengthFn(value) {
   } else if (value > 60) {
     titleLength.innerHTML = `<span>Độ dài TITLE là</span>: ${value} > 60. Không tốt :(`;
   } else if (value == 0) {
-    titleLength.innerHTML = `<span>Độ dài TITLE là</span>: Chưa nhập`;
+    titleLength.innerHTML = `<span>Độ dài TITLE là</span>: Vui lòng nhập title.`;
   }
 }
 
